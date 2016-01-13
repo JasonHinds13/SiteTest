2a9753333eaeb0794d9d28190367b3fcd6d7979b
JasonHinds13 <brandonhinds13@gmail.com>
2016-01-13 00:26:05 -0500

    Update main.py
    removed object property module since .ids removed the need for it.

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
+#If on Windows to avoid fullscreen, use the following two lines of code
+#from kivy.config import Config
+#Config.set('graphics', 'fullscreen', '0')
+
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
 
 from urllib2 import Request, urlopen
 from socket import socket, gethostbyname, AF_INET, SOCK_STREAM
 
 Builder.load_string('''
 <TestSite>:
-    t_ftp: ftp
-    t_addr: addr
-    t_status: status
-    t_website: website
-        
+    
     TextInput:
         id: website
         pos: 0,root.top-50
@@ -45,18 +44,13 @@ Builder.load_string('''
 ''')
 
 class TestSite(Widget):
-    
-    t_ftp = ObjectProperty(None)
-    t_addr = ObjectProperty(None)
-    t_status = ObjectProperty(None)
-    t_website = ObjectProperty(None)
 
     #This function tests port 21 for an open ftp port
     #If it is open an ftp attack is likely
     
     def ftest(self):
         try:
-            url = self.t_website.text
+            url = self.ids.website.text
             url = url.replace("http://www.", "")
             url = url.split("/")
             
@@ -64,18 +58,18 @@ class TestSite(Widget):
             test = sock.connect_ex((url[0],21))
             
             if test == 0:
-                self.t_ftp.text  = "Port 21 is Open. Site May Be Vunerable to FTP Attacks"
-                self.t_ftp.color = (0, 1, 0, 1)
+                self.ids.ftp.text  = "Port 21 is Open. Site May Be Vunerable to FTP Attacks"
+                self.ids.ftp.color = (0, 1, 0, 1)
                 
             else:
-                self.t_ftp.text  = "Port 21 is Close. Site Not Vunerable to FTP Attacks"
-                self.t_ftp.color = (1, 0, 0, 1)
+                self.ids.ftp.text  = "Port 21 is Close. Site Not Vunerable to FTP Attacks"
+                self.ids.ftp.color = (1, 0, 0, 1)
                     
             sock.close()
             
         except:
-            self.t_ftp.text  = "Unable to Test for FTP Attack"
-            self.t_ftp.color = (1, 0, 0, 1)
+            self.ids.ftp.text  = "Unable to Test for FTP Attack"
+            self.ids.ftp.color = (1, 0, 0, 1)
 
     #This tests a php webpage for any possibility of an SQL injection attack
     #If it is vunerable it will give the user the ip address of the website
@@ -83,33 +77,33 @@ class TestSite(Widget):
     def stest(self):
         
         try:
-            url = self.t_website.text
+            url = self.ids.website.text
             req = Request(url+"?id=1'")
 
             response = urlopen(req)
             page = response.read()
 
             if ("Error" in page) and ("SQL" in page):
-                self.t_status.text = "Site May be Vunerable to SQL injection Attacks"
-                self.t_status.color = (0, 1, 0, 1)
+                self.ids.status.text = "Site May be Vunerable to SQL injection Attacks"
+                self.ids.status.color = (0, 1, 0, 1)
                 
                 url = url.replace("http://www.", "")
                 url = url.split("/")
-                self.t_addr.text = "ip address: %s" %gethostbyname(url[0])
+                self.ids.addr.text = "ip address: %s" %gethostbyname(url[0])
 
             else:
-                self.t_addr.text = ""
-                self.t_status.text = "No SQL Vunrabilities Detected"
-                self.t_status.color = (1, 0, 0, 1)
+                self.ids.addr.text = ""
+                self.ids.status.text = "No SQL Vunrabilities Detected"
+                self.ids.status.color = (1, 0, 0, 1)
                 
         except:
-            self.t_status.text = """
+            self.ids.status.text = """
             Make sure the site is in the format:
                http://www.example.com/page.php
             If you are testing for an SQL Vunrability
             
             (Also Check Your Internet Connection)"""
-            self.t_status.color = (1, .5, 0, 1)
+            self.ids.status.color = (1, .5, 0, 1)
 
     #This will call both test functions when the 'Test' button is pressed
             
