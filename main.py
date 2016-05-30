a33e04a2e46ed5d5ba72543587b5962edda89c1e
Brandon Hinds <brandonhinds13@gmail.com>
2016-05-30 14:40:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,56 @@
-#If on Windows to avoid fullscreen, use the following two lines of code
+#If on Windows, to avoid fullscreen use the following two lines of code
 #from kivy.config import Config
 #Config.set('graphics', 'fullscreen', '0')
 
 #------ Notes ------#
-# Port Scan Slow on Windows
-# Test Button Should Become a Drop Menu for Separate Tests
+# Port Scan May Be Slow
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
+from kivy.uix.dropdown import DropDown
 
 from urllib2 import Request, urlopen
 from socket import socket, gethostbyname, AF_INET, SOCK_STREAM
 
 Builder.load_string('''
+<TestsMenu>:
+
+    Button:
+        id: sqlbtn
+        text: 'SQL Test'
+        size_hint_y: None
+        height: 50
+        background_color: 0,.5,1,1
+
+    Button:
+        id: ftpbtn
+        text: 'FTP Test'
+        size_hint_y: None
+        height: 50
+        background_color: 0,.5,1,1
+
+    Button:
+        id: portbtn
+        text: 'Port Scan'
+        size_hint_y: None
+        height: 50
+        background_color: 0,.5,1,1
+
 <TestSite>:
-    
+
     TextInput:
         id: website
         pos: 0,root.top-50
         size: root.width-200,50
+
     Button:
-        text: 'Test'
+        text: 'Tests'
         size: 200,50
         background_color: 0,.5,1,1
         pos: root.width-200, root.top-50
-        
-        on_press:
-            status.text = "Testing For Vunrabilities..."
-            addr.text = ""
-            ftp.text = ""
-            sport.text = ""
-            status.color = (1, .5, 0, 1)
-        
-        on_release: root.test()
-        
+        on_release: root.menu.open(self)
+
     GridLayout:
         cols: 1
         pos: root.center
@@ -42,22 +58,22 @@ Builder.load_string('''
         Label:
             id: status
             text: "Test Websites for SQL Vunrabilities"
-        
         Label:
-            id: addr
-            text: ""
-        Label:
-            id: ftp
-            text: ""
-        Label:
-            id: sport
+            id: info
             text: ""
 
 ''')
 
+class TestsMenu(DropDown):
+    pass
+
 class TestSite(Widget):
 
-    def portScan(self):
+    menu = TestsMenu() #The dropdown menu for each test
+
+    #This function checks for open ports
+
+    def portScan(self, btn):
         try:
             url = self.ids.website.text
             url = url.replace("http://www.", "")
@@ -65,8 +81,8 @@ class TestSite(Widget):
 
             sock = socket(AF_INET, SOCK_STREAM)
 
-            self.ids.sport.text = "Open Ports: "
-            self.ids.sport.color = (0, 1, 0, 1)
+            self.ids.status.text = "Open Ports: "
+            self.ids.status.color = (0, 1, 0, 1)
 
             for port in range(1, 1025):
                 test = sock.connect_ex((url[0],port))
@@ -74,47 +90,52 @@ class TestSite(Widget):
                 #If port is open - port number is concatinated to string
                 if test == 0:
                     s = str(port) + ", "
-                    self.ids.sport.text  += s
-                        
+                    self.ids.status.text  += s
+
             sock.close()
-                
+
         except:
-            self.ids.sport.text = "Error - No Port Scan available"
-            self.ids.sport.color = (1, 0, 0, 1)
+            self.ids.status.text = "Error - No Port Scan available"
+            self.ids.status.color = (1, 0, 0, 1)
 
     #This function tests port 21 for an open ftp port
     #If it is open an ftp attack is likely
-    
-    def ftest(self):
+
+    def ftpTest(self, btn):
         try:
             url = self.ids.website.text
             url = url.replace("http://www.", "")
             url = url.split("/")
-            
+
             sock = socket(AF_INET, SOCK_STREAM)
             test = sock.connect_ex((url[0],21))
-            
+
             if test == 0:
-                self.ids.ftp.text  = "Port 21 is Open. Site May Be Vunerable to FTP Attacks"
-                self.ids.ftp.color = (0, 1, 0, 1)
-                
+                self.ids.status.text  = "Port 21 is Open. Site May Be Vunerable to FTP Attacks"
+                self.ids.status.color = (0, 1, 0, 1)
+
             else:
-                self.ids.ftp.text  = "Port 21 is Close. Site Not Vunerable to FTP Attacks"
-                self.ids.ftp.color = (1, 0, 0, 1)
-                    
+                self.ids.status.text  = "Port 21 is Close. Site Not Vunerable to FTP Attacks"
+                self.ids.status.color = (1, 0, 0, 1)
+
             sock.close()
-            
+
         except:
-            self.ids.ftp.text  = "Unable to Test for FTP Attack"
-            self.ids.ftp.color = (1, 0, 0, 1)
+            self.ids.status.text  = "Unable to Test for FTP Attack"
+            self.ids.status.color = (1, 0, 0, 1)
 
     #This tests a php webpage for any possibility of an SQL injection attack
     #If it is vunerable it will give the user the ip address of the website
-            
-    def stest(self):
-        
+
+    def sqlTest(self, btn):
+
         try:
             url = self.ids.website.text
+
+            if "http://www." not in url:
+                url.strip("www.") #if www. is at the beginning
+                url = "http://www."+url
+
             req = Request(url+"?id=1'")
 
             response = urlopen(req)
@@ -123,40 +144,52 @@ class TestSite(Widget):
             if ("Error" in page) and ("SQL" in page):
                 self.ids.status.text = "Site May be Vunerable to SQL injection Attacks"
                 self.ids.status.color = (0, 1, 0, 1)
-                
+
                 url = url.replace("http://www.", "")
                 url = url.split("/")
-                self.ids.addr.text = "ip address: %s" %gethostbyname(url[0])
+                self.ids.info.text = "ip address: %s" %gethostbyname(url[0])
 
             else:
-                self.ids.addr.text = ""
                 self.ids.status.text = "No SQL Vunrabilities Detected"
                 self.ids.status.color = (1, 0, 0, 1)
-                
+
         except:
-            self.ids.status.text = """
-            Make sure the site is in the format: http://www.example.com/page.php
-                        If you are testing for an SQL Vunrability            
-                           (Also Check Your Internet Connection)"""
+            self.ids.status.text = "Make sure the site is in the format: http://www.example.com/page.php"
             self.ids.status.color = (1, .5, 0, 1)
 
-    #This will call all test functions when the 'Test' button is pressed
-            
-    def test(self):
-        self.stest()
-        self.ftest()
-        self.portScan()
-    
+    #This will link the buttons from the dropdown menu to functions
+
+    def bindButtons(self):
+
+        def process(mes):
+            self.ids.status.text = mes
+            self.ids.info = ""
+            self.ids.status.color = (1, .5, 0, 1)
+
+        self.menu.ids.sqlbtn.bind(on_press=process("Testing For SQL Vunrabilities..."))
+        self.menu.ids.ftpbtn.bind(on_press=process("Testing For FTP Vunrabilities..."))
+        self.menu.ids.portbtn.bind(on_press=process("Scanning For Open Ports..."))
+
+        self.menu.ids.sqlbtn.bind(on_release=self.sqlTest)
+        self.menu.ids.ftpbtn.bind(on_release=self.ftpTest)
+        self.menu.ids.portbtn.bind(on_release=self.portScan)
+
+        if self.ids.website.text == "":
+            self.ids.status.text = "Test Websites for SQL Vunrabilities"
+            self.ids.status.color = (1,1,1,1)
+
 class TestSiteApp(App):
-    
+
     def build(self):
-        return TestSite()
-        
+        testSite = TestSite()
+        testSite.bindButtons()
+        return testSite
+
     def on_pause(self):
         return True
-        
+
     def on_resume(self):
         pass
-        
+
 if __name__ == "__main__":
     TestSiteApp().run()
